cabb5bd07691a40c7fc62dfde7debb14d6cbc06f
prestonmeek <prestonmeek6@gmail.com>
2024-12-19 01:17:23 -0500

    fixed pointer formatting

--- Core/Src/microsd.c
+++ Core/Src/microsd.c
@@ -7,9 +7,9 @@
 
 #include "microsd.h"
 
-static SD_HandleTypeDef* sd_handle;
+static SD_HandleTypeDef *sd_handle;
 
-microsd_ret_t MicroSD_Open(SD_HandleTypeDef* handle) {
+microsd_ret_t MicroSD_Open(SD_HandleTypeDef *handle) {
     if (HAL_SD_Init(handle) != HAL_OK)
     {
         return MICROSD_ERROR_UNABLE_TO_OPEN;
